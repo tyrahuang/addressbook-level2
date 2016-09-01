f01b273b04dc80daf52818293098bd628b98176a
TYRA-PC\tyra <tyrack99@gmail.com>
2016-09-01 19:12:22 +0800

    T3A2 Ready for review

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -13,10 +13,14 @@ public class CommandResult {
     /** The feedback message to be shown to the user. Contains a description of the execution result */
     private final String feedbackToUser;
     
-
+    public String ReturnfeedbackToUser(){
+    	return feedbackToUser;
+    }
+    
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
+    
     public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
         relevantPersons = null;
