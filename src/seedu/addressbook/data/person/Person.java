a8611a63a0660ee1d0cfd8beb34fa9324ac3947c
TYRA-PC\tyra <tyrack99@gmail.com>
2016-09-06 16:46:02 +0800

    T4A2

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,6 +14,9 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    
+    private int sequenceNumber = 0;
+    private static int nextSequenceNumber = 1;
 
     private final UniqueTagList tags;
     /**
@@ -25,6 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.sequenceNumber = nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
@@ -65,7 +70,14 @@ public class Person implements ReadOnlyPerson {
     public void setTags(UniqueTagList replacement) {
         tags.setTags(replacement);
     }
-
+    
+    public int getSequenceNumber(){
+    	return sequenceNumber;
+    }
+    
+    public static int getNextSequenceNumber(){
+    	return nextSequenceNumber;
+    }
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
